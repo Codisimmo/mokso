77cc57727537877b80bcb6cf9dd3c04a746e5286
Jan Kučera <93750764+SharkJohny@users.noreply.github.com>
2025-12-22 00:45:18 +0100

    Add category menu and enhance homepage layout
    Introduces a new category menu HTML component and related SCSS styles for improved layout and responsiveness. Updates homepage and product detail JS to restructure article and banner placement, and applies a slick slider to homepage articles for better presentation.

--- src/js/pages/product-detail.js
+++ src/js/pages/product-detail.js
@@ -1,3 +1,6 @@
 export default initProductDetailPage;
 
-function initProductDetailPage() {}
+function initProductDetailPage() {
+  $(".products.products-block.products-related.products-additional.p-switchable").insertAfter(".shp-tabs-wrapper.p-detail-tabs-wrapper");
+  $("h2.products-related-header").insertAfter(".shp-tabs-wrapper.p-detail-tabs-wrapper");
+}
