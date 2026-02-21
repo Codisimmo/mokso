9b15a89f63087d42503e4224819c0123abbf01f3
Jakub Dostál <info@mokso.cz>
2026-02-21 03:38:04 +0100

    Hide social buttons on product detail

--- src/js/pages/product-detail.js
+++ src/js/pages/product-detail.js
@@ -1,6 +1,12 @@
 export default initProductDetailPage;
 
 function initProductDetailPage() {
-  $(".products.products-block.products-related.products-additional.p-switchable").insertAfter(".shp-tabs-wrapper.p-detail-tabs-wrapper");
-  $("h2.products-related-header").insertAfter(".shp-tabs-wrapper.p-detail-tabs-wrapper");
+  $(".products.products-block.products-related.products-additional.p-switchable")
+    .insertAfter(".shp-tabs-wrapper.p-detail-tabs-wrapper");
+
+  $("h2.products-related-header")
+    .insertAfter(".shp-tabs-wrapper.p-detail-tabs-wrapper");
+
+  // Schovej social buttons na detailu produktu
+  $(".social-buttons-wrapper, .link-icons[data-testid='productDetailActionIcons'], .social-buttons").hide();
 }
