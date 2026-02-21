fa800ee1ec11c25da4a1fcaeace7776e22dbd907
Jakub Dostál <info@mokso.cz>
2026-02-21 04:18:01 +0100

    Show rating under description

--- src/js/pages/product-detail.js
+++ src/js/pages/product-detail.js
@@ -6,12 +6,4 @@ function initProductDetailPage() {
 
   $("h2.products-related-header")
     .insertAfter(".shp-tabs-wrapper.p-detail-tabs-wrapper");
-
-  // Přesun hodnocení (hvězdy + počet) pod krátký popis
-  const $rating = $(".p-detail-rating, .rating-wrapper, .stars-wrapper").first();
-  const $desc = $(".p-short-description, .p-detail-info .perex, .p-detail-info").first();
-
-  if ($rating.length && $desc.length) {
-    $rating.insertAfter($desc);
-  }
 }
